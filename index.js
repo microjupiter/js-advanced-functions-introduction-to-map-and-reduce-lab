df9a16b489b8c28cca0b9bf7c0f96019c788869c
Kyle Wilson <kyle@hinterlandstills.com>
2021-03-05 11:29:00 -0600

    commit tree a9e339e44fd4c34ea91092299b7fca1adbc63de4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function mapToNegativize(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i])
+  }
+  return r
+}
+function mapToNoChange(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++) {
+    r.push(src[i])
+  }
+  return r 
+}
